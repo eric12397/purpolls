27f7590ea7fb895be0716b72662443cb90a9627f
unknown <eric12397@gmail.com>
2020-08-27 01:44:20 -0400

    update gitignore

---
+++ build/precache-manifest.21f3b1b54f7e2290565c99909b20ac21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b79447844397e64f5e0ac18e49b9c56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f383764ae532a5fee73",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b78a45afad626d6b173f",
+    "url": "/static/css/main.c9bab546.chunk.css"
+  },
+  {
+    "revision": "6f383764ae532a5fee73",
+    "url": "/static/js/2.676a76fa.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/static/js/2.676a76fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b78a45afad626d6b173f",
+    "url": "/static/js/main.624b7ae5.chunk.js"
+  },
+  {
+    "revision": "68a9665f6992de0c4239",
+    "url": "/static/js/runtime-main.fc75219d.js"
+  }
+]);
