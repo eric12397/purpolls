d86c139bcbb02a50f525a92c92b48a853a4f9b99
unknown <eric12397@gmail.com>
2020-09-01 01:55:23 -0400

    updated settings.py

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,10 +4,16 @@ import { motion } from 'framer-motion';
 
 class Home extends React.Component {
   render() {
-                          
+    let welcomeMsg = '';
+    if (this.props.location.state !== undefined) {
+      welcomeMsg =  
+      <UncontrolledAlert className="alert">
+        {this.props.location.state.message}
+      </UncontrolledAlert>
+    }
+
     return (
       <motion.div
-        
         animate={{opacity: 1, y: 0}}
         exit={{opacity: 0, y: '100vh'}}
         transition={ pageTransition } 
