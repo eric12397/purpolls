5eb07e78f1e05dc8847cdce90358d8844d38476c
unknown <eric12397@gmail.com>
2020-09-01 01:29:15 -0400

    updated package.json and Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,9 @@
 import React from 'react'
 import PollList from "./polls/PollList"
-import { UncontrolledAlert } from 'reactstrap';
 import { motion } from 'framer-motion';
 
 class Home extends React.Component {
   render() {
-    let welcomeMsg = '';
-    if (this.props.location.state !== undefined) {
-      welcomeMsg =  
-      <UncontrolledAlert className="alert">
-        {this.props.location.state.message}
-      </UncontrolledAlert>
-    }
                           
     return (
       <motion.div
