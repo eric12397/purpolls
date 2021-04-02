3e9c2713b7e92c764d808e2f4600abc4fab8479d
Eric Choi <eric12397@gmail.com>
2021-04-02 01:03:46 -0400

    added initial fixture data for tests

--- polls/tests.py
+++ polls/tests.py
@@ -2,5 +2,15 @@ import datetime
 from django.test import TestCase
 from django.utils import timezone
 from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+from .models import Poll
 
 # Create your tests here.
+class MyTest(TestCase):
+    fixtures = ["polls/fixtures/poll.json"]
+
+    def test_should_get_fixture(self):
+        poll = Poll.objects.get(pk=1)
+        self.assertEqual(poll.question_text, "Who's winning NBA MVP this year?")
+        self.assertEqual(poll.author.username, "Gordigante")
